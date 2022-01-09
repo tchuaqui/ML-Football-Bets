98d1f3d92774fc3ae2375c75ad1a23b883164094
Luís Calejo <luis@calejo.com>
2022-01-09 19:49:59 +0100

    Refactored script to read odds data

--- src/get-data/odds/get_odds_oddsmagnet.py
+++ src/get-data/odds/get_odds_oddsmagnet.py
@@ -2,6 +2,7 @@ import urllib.request
 import json
 import pandas as pd
 
+league = 'england-premier-league'
 years = [2021, 2022]
 bet_types = ['win-market']
 teams = [
@@ -12,29 +13,31 @@ teams = [
     'queens-park-rangers'
 ]
 
+
+def get_data_oddsmagnet(league, years, bet_types, teams):
+    for year in years:
+        for bet_type in bet_types:
+            odds_data = []
+            odds_columns = []
+            for team_home in teams:
+                for team_away in teams:
+                    if team_home != team_away:
+                        link = "https://data.oddsmagnet.com/history/%s/football/%s/%s-v-%s/%s.json" % \
+                               (year, league, team_home, team_away, bet_type)
+                        try:
+                            with urllib.request.urlopen(link) as url:
+                                odds = json.loads(url.read().decode())
+                                odds_data_new = odds["data"]
+                                odds_data += odds_data_new
+                                if not odds_columns:
+                                    odds_columns = odds["columns"]
+                                print("%s/%s-v-%s/%s" % (year, team_home, team_away, bet_type))
+                        except:
+                            continue  # if url does not return any files then we ignore it
+            df_odds = pd.DataFrame(data=odds_data, columns=odds_columns)
+            df_odds.to_csv('../../../data/odds/%s_%s_%s.csv' % (year, league, bet_type))
+
+
 print("START")
-
-for year in years:
-    odds_data = []
-    odds_columns = []
-    for bet_type in bet_types:
-        for team_home in teams:
-            for team_away in teams:
-                if team_home != team_away:
-                    link = "https://data.oddsmagnet.com/history/%s/football/england-premier-league/%s-v-%s/%s.json" % \
-                           (year, team_home, team_away, bet_type)
-                    try:
-                        with urllib.request.urlopen(link) as url:
-                            odds = json.loads(url.read().decode())
-                            odds_data_new = odds["data"]
-                            odds_data += odds_data_new
-                            if not odds_columns:
-                                odds_columns = odds["columns"]
-                            print("%s/%s-v-%s/%s"%(year, team_home, team_away, bet_type))
-                    except:
-                        continue
-    df_odds = pd.DataFrame(data=odds_data, columns=odds_columns)
-    df_odds.to_csv('OddsHistory_%s.csv' % year, index=False)
-
+get_data_oddsmagnet(league, years, bet_types, teams)
 print("FINISH")
-
