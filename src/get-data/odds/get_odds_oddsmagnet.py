3faa860c8402d901d77dd936854b7e9f5d77816d
Luís Calejo <luis@calejo.com>
2022-01-09 02:19:38 +0100

    updated script to get odds historical data from oddsmagnet

--- src/get-data/odds/get_odds_oddsmagnet.py
+++ src/get-data/odds/get_odds_oddsmagnet.py
@@ -2,9 +2,39 @@ import urllib.request
 import json
 import pandas as pd
 
-link = "https://data.oddsmagnet.com/history/2021/football/england-premier-league/leeds-united-v-brentford/win-market.json"
+years = [2021, 2022]
+bet_types = ['win-market']
+teams = [
+    'arsenal', 'aston-villa', 'brentford', 'brighton', 'burnley', 'chelsea', 'crystal-palace', 'everton',
+    'leeds-united', 'leicester-city', 'liverpool', 'manchester-city', 'manchester-united', 'norwich-city' 'newcastle',
+    'southampton', 'tottenham', 'watford', 'west-ham', 'wolves', 'sheffield-united', 'west-bromwich', 'fulham',
+    'bournemouth', 'cardiff-city', 'huddersfield', 'swansea', 'stoke-city', 'hull-city', 'middlesbrough', 'sunderland',
+    'queens-park-rangers'
+]
 
-with urllib.request.urlopen(link) as url:
-    matches = json.loads(url.read().decode())
-    matches_df = pd.DataFrame(data = matches["data"], columns= matches["columns"])
+print("START")
+
+for year in years:
+    odds_data = []
+    odds_columns = []
+    for bet_type in bet_types:
+        for team_home in teams:
+            for team_away in teams:
+                if team_home != team_away:
+                    link = "https://data.oddsmagnet.com/history/%s/football/england-premier-league/%s-v-%s/%s.json" % \
+                           (year, team_home, team_away, bet_type)
+                    try:
+                        with urllib.request.urlopen(link) as url:
+                            odds = json.loads(url.read().decode())
+                            odds_data_new = odds["data"]
+                            odds_data += odds_data_new
+                            if not odds_columns:
+                                odds_columns = odds["columns"]
+                            print("%s/%s-v-%s/%s"%(year, team_home, team_away, bet_type))
+                    except:
+                        continue
+    df_odds = pd.DataFrame(data=odds_data, columns=odds_columns)
+    df_odds.to_csv('OddsHistory_%s.csv' % year, index=False)
+
+print("FINISH")
 
