edeb58d1cd90936df96a8ce183f904b214fc4751
Luís Calejo <luis@calejo.com>
2022-01-14 22:33:23 +0100

    Refactored oddsmagnet script. Now using master data to get list of leagues and teams

--- src/get-data/odds/get_data_from_oddsmagnet.py
+++ src/get-data/odds/get_data_from_oddsmagnet.py
@@ -2,17 +2,11 @@ import urllib.request
 import json
 import pandas as pd
 
-league = 'england-premier-league'
 years = [2021, 2022]
 bet_types = ['win-market']
-teams = [
-    'arsenal', 'aston-villa', 'brentford', 'brighton', 'burnley', 'chelsea', 'crystal-palace', 'everton',
-    'leeds-united', 'leicester-city', 'liverpool', 'manchester-city', 'manchester-united', 'norwich-city' 'newcastle',
-    'southampton', 'tottenham', 'watford', 'west-ham', 'wolves', 'sheffield-united', 'west-bromwich', 'fulham',
-    'bournemouth', 'cardiff-city', 'huddersfield', 'swansea', 'stoke-city', 'hull-city', 'middlesbrough', 'sunderland',
-    'queens-park-rangers'
-]
 
+
+# Note: List of leagues and teams comes from data/mapping
 
 def get_data_oddsmagnet(league, years, bet_types, teams):
     for year in years:
@@ -35,9 +29,13 @@ def get_data_oddsmagnet(league, years, bet_types, teams):
                         except:
                             continue  # if url does not return any files then we ignore it
             df_odds = pd.DataFrame(data=odds_data, columns=odds_columns)
-            df_odds.to_csv('../../../data/odds/%s_%s_%s.csv' % (year, league, bet_type))
+            df_odds.to_csv('../../../data/oddsmagnet/%s_%s_%s.csv' % (year, league, bet_type))
 
 
 print("START")
-get_data_oddsmagnet(league, years, bet_types, teams)
+df_teams = pd.read_csv('../../../data/master-data/teams.csv')
+df_teams.dropna(subset=["oddsmagnet_team"], inplace=True)
+league_teams = df_teams.groupby('oddsmagnet_league')['oddsmagnet_team'].apply(list)
+for league in league_teams.index:
+    get_data_oddsmagnet(league, years, bet_types, league_teams.loc[league])
 print("FINISH")
